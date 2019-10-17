e707d36ac3087ed4066305a6aee88218f843469b
Maria Kuzminyh <m.polynskaya@yandex.ru>
2019-10-17 13:34:24 +0500

    +

--- js/portfolio.js
+++ js/portfolio.js
@@ -5,7 +5,6 @@ function alignmentOfHeightsOfTwoBlocks() {
     document.querySelector(".portfolioObjects").style.height = portfolioObjectsHeight + 'px';
 };
 
-//
 let portfolio = {
     idOfportfolioObject: [ ...document.querySelectorAll('.portfolioObject__')],
     idOfportfolioObjects: [ ...document.querySelector(".portfolioObjects").querySelectorAll('img')],
@@ -16,7 +15,6 @@ let portfolio = {
         this.idOfportfolioObjects.forEach(function(item, i) {
             item.addEventListener('click', function(event) {
                 portfolio.changeActiveObject(i);
-                //alert(i);
             });
         });
     },
@@ -29,35 +27,6 @@ let portfolio = {
 
     
 };
-
-    /*choseIt() {
-        //let portfolioObjects = document.querySelector(".portfolioObjects");
-        this.portfolioObjects.querySelectorAll('img');
-        for (let i = 0; i < this.portfolioObjects.length; i++) {
-            this.portfolioObjects.addEventListener('click', function(event) {
-                alert(this.idOfportfolioObjects.event.target.id);
-            });
-        }
-
-    },
-
-    openDesiredPortfolioObject() {
-
-        
-        
-    } */
-
-/* portfolioObjects.querySelector('img').addEventListener('click', function(event) {
-    alert(idOfportfolioObjects.indexOf("event.target.id"));
-    //let thisObject = document.querySelector('#' + idOfportfolioObject[idOfportfolioObjects.indexOf("event.target.id")]);
-    
-}); */
-
-/* .addEventListener('click', function(event) {
-    event.stopPropagation();
-    product.name = buttonOfAddToBasket[i].parentNode.querySelector('h3').textContent;
-    basket.putProduct(product);
-}); */
  
 alignmentOfHeightsOfTwoBlocks();
 portfolio.choseIt();
