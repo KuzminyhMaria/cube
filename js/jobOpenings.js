276c2f1db7131178e79d9732a01af0cd93771468
Maria Kuzminyh <m.polynskaya@yandex.ru>
2019-10-29 16:36:48 +0500

    +

--- js/jobOpenings.js
+++ js/jobOpenings.js
@@ -18,30 +18,3 @@ let jobOpenings = {
 document.addEventListener('DOMContentLoaded', function(event) {
     jobOpenings.choseIt();
 });
-
-let upArrowForLifting = {
-    upArrow: document.querySelector('.upArrowForLifting'),
-
-    clickOn() {
-        this.upArrow.addEventListener('click', function(event) {
-            window.scrollTo({
-                top: 0,
-                behavior: 'smooth'
-            });
-        });
-    },
-
-    upArrowStateChange() {
-        if (window.pageYOffset >= 400) {
-            upArrowForLifting.upArrow.classList.remove("notActive");
-        } else {
-            upArrowForLifting.upArrow.classList.add("notActive");
-        }
-    }
-};
-
-document.addEventListener('scroll', function(event) {
-    upArrowForLifting.upArrowStateChange();
-});
-
-upArrowForLifting.clickOn();
